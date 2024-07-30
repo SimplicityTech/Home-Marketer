f4c926410fb1cd594ce4ff082f6a9ac0d66faee8
SimplicityTech <agcobileliwani5@gmail.com>
2024-07-30 22:52:22 +0200

    Update status.js

--- status.js
+++ status.js
@@ -1,3 +1,5 @@
+import { GithubAuthProvider } from 'firebase/auth';
+const provider = new GithubAuthProvider();
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
