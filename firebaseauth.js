04ad97c60776d2c14eb41e71fc51f90782facb8d
SimplicityTech <agcobileliwani5@gmail.com>
2025-03-24 16:32:32 +0200

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {getAuth, sendPasswordResetEmail, signInWithEmailPassword} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
